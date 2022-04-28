426905ae8a45f4dc60cf19308ebdec7ea8005be0
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-04-28 14:59:06 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n,sum=0;     cin>>n;     while(n)     {         sum+=n%10;         n/=10;         if(sum>9 and n==0)         {             n=sum;             sum=0;         }     }     cout<<sum; }

---
+++ Add_Digits.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cin>>n;
+    while(n)
+    {
+        sum+=n%10;
+        n/=10;
+        if(sum>9 and n==0)
+        {
+            n=sum;
+            sum=0;
+        }
+    }
+    cout<<sum;
+}
