64281e8b533207517f0a866c04cddbbe366dcab1
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-04-30 22:40:22 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     int arr[n][m];     for(int i=0,sum;i<n;i++)     {         sum=0;         for(int j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);             sum+=arr[i][j];         }         printf("%d ",sum);     } }

---
+++ Row_wise_sum_of_a_matrix.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int arr[n][m];
+    for(int i=0,sum;i<n;i++)
+    {
+        sum=0;
+        for(int j=0;j<m;j++)
+        {
+            scanf("%d",&arr[i][j]);
+            sum+=arr[i][j];
+        }
+        printf("%d ",sum);
+    }
+}
